077a9cbc680b3af6002a5c87b819b67004903ee5
Raj Rampratap Sharma <117432972+rajsharma07@users.noreply.github.com>
2025-03-20 12:05:25 +0530

    Execute updated

--- execute.sh
+++ execute.sh
@@ -2,5 +2,5 @@ sudo apt install python3 python3-pip uvicorn
 sudo apt install python3-venv
 python3 -m venv myenv
 . myenv/bin/activate
-pip3 install fastapi transfromers torch SentencePiece spacy
+pip3 install fastapi uvicorn transformers torch
 uvicorn main:app --reload
