198f95a7fc0e9e2cb5579eeba4dfd0254def61c8
Raj Rampratap Sharma <117432972+rajsharma07@users.noreply.github.com>
2025-03-20 12:56:19 +0530

    Updated execute.sh

--- execute.sh
+++ execute.sh
@@ -1,6 +1,9 @@
 sudo apt install python3 python3-pip uvicorn
+curl -fsSL https://ollama.com/install.sh | sh
+ollama pull codellama
 sudo apt install python3-venv
 python3 -m venv myenv
 . myenv/bin/activate
 pip3 install fastapi uvicorn transformers torch
-uvicorn main:app --reload
+uvicorn main:app --host 0.0.0.0 --port 8000
+
